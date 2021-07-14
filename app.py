a13d71fe40ca8d485a74083d91957ae3c531a975
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-07-13 20:05:24 -0700

    This assignment is a work in progress

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
-from flask import Flask, request, render_template, flash, redirect, jsonify, session
+from flask import Flask, request, render_template, flash, redirect
 #from random import randint,  choice, sample
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import satisfaction_survey as survey
 
-responses = 'responses'
+
+#memorize[stored_responses] = []
+# stored_responses = 'responses'
+responses = []
+stored_responses = 0
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-
 debug = DebugToolbarExtension(app)
 
-stored_responses = 'responses'
-#memorize[stored_responses] = []
+
 
 @app.route('/')
 def show_survey_title():
-    """Shows survey title"""
-    return render_template('base.html')
-
-@app.route('/start', methods=['POST'])
+    return render_template('start.html')
+    
+    
+@app.route('/start')
 def start_survey():
-    memorized[stored_responses] = []
-    return redirect('/questions/0')
-
-@app.route('/answer', methods=['POST'])
-def question_handel():
-        return render_template('answer.html')
-
-
-@app.route('/thanks')
-def say_thanks():
-    """Thanks user"""
-    return render_template('thanks.html')
+    """Shows """
+    return render_template('start.html', survey=survey)
+ 
+    # return redirect('/questions/0')
+    # instructions = survey.instructions    
+    # title =survey.questions
+    # if(len(responses) == len(survey.questions)):
+    #     redirect('/questions/0')
+    # else:
+    #     return render_template('start.html', instructions=instructions, title=title)
 
 
+
+# @app.route('/question/<int:qid>')
+# def question_handle(qid):
+#         return render_template('answer.html')
+
+# @app.route('/thanks')
+# def thank_user():
+#     return render_template('thanks.html')
+        
