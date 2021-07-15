5233d812f2a4c1dd975998aef66e9325b0d93d5d
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-07-14 20:58:31 -0700

    Survey form still not showing up

--- app.py
+++ app.py
@@ -20,40 +20,44 @@ def show_survey():
     return render_template('start.html')
     
     
-@app.route('/start', methods=['POST'])
+@app.route('/start')
 def start_survey():
     """Shows survey title and instructions """
-    session[stored_responses] = []
+    # session[stored_responses] = []
+    # title = survey.title
+    # instructions = survey.instructions
     return redirect('/questions/0')
-
-@app.route('/answer', methods=['POST'])
-def handle_answer():
-    choice = request.form['answer']
-    responses = session['stored_responses']
-    responses.append('choice')
-    session['stored_responses'] = responses
-
-    if (len(responses) == len(survey.questions)):
-        return redirect('/thanks')
-    else:
-        return redirect(f"/questions/{len(responses)}")
+    # return render_template('questions.html')
 
 @app.route('/questions/<int:qid>')
 def show_questions(qid):
     responses = session.get(stored_responses)
     if(responses is None):
+        # flash('Sorry you cannot do that')
         return redirect('/')
         
     if(len(responses) == len(survey.questions)):
         return redirect('/thanks')
         """Redirects user if they try to manually change url"""
     if(len(responses) != qid):
-        flash(f"Invalid question id: {qid}.")
+        flash(f'Invalid question id: {qid}. You cannot do that!')
         return redirect(f"/questions/{len(responses)}")
     
     question = survey.questions[qid]
     return render_template(
-        "questions.html", question_num=qid, question=question)
+        "questions.html", question_num=qid, questions=questions)
+
+@app.route('/answer', methods=['POST'])
+def handle_answer():
+    choice = request.form['answer']
+    responses = session[stored_responses]
+    responses.append('choice')
+    session[stored_responses] = responses
+
+    if (len(responses) == len(survey.questions)):
+        return redirect('/thanks')
+    else:
+        return redirect(f"/questions/{len(responses)}")
 
 
 @app.route('/thanks')
